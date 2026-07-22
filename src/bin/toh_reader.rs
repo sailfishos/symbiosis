fb0aef6fb87e21b59477bfc366948323575cd1ac
Tomi Leppänen <tomi.leppanen@jolla.com>
2026-07-22 13:58:02 +0300

    toh_reader: Use the library for reading TOHs
    Works mostly the same as before.
    
    Signed-off-by: Tomi Leppänen <tomi.leppanen@jolla.com>

--- src/bin/toh_reader.rs
+++ src/bin/toh_reader.rs
@@ -3,142 +3,83 @@
 //! TOH memory chip reader.
 
 use argh::FromArgs;
-use libc::{self, ioctl};
 use std::fs::File;
-use std::io::{self, Read, Seek, Write};
-use std::os::fd::AsRawFd;
-use std::thread::sleep;
+use std::io::Write;
 use std::time::Duration;
+use tokio::{select, time::sleep};
 
-use symbiosis::back_cover::paths::{ADC_PATH, I2C_PATH, INT_PATH, PWR_PATH};
-use symbiosis::i2cdev::I2C_SLAVE;
+use symbiosis::back_cover::{BackCover, Variant};
 
 /// TOH memory chip reader.
+///
+/// Supports chips with 256 byte blocks.
 #[derive(FromArgs)]
+#[argh(help_triggers("-h", "--help"))]
 struct Arguments {
     /// output file.
     #[argh(positional)]
     output_file: String,
+    /// force overwrite
+    #[argh(switch, short = 'f', long = "force")]
+    overwrite: bool,
 }
 
-/// Wait for INT pin to become 0
-fn wait_for_int() -> Result<(), std::io::Error> {
-    let mut int = File::open(INT_PATH)?;
-    let mut lock = std::io::stdout().lock();
-    write!(lock, "Waiting for INT pin to go low for up to a minute")?;
-    lock.flush()?;
-    for _ in 0..(60_000 / 500) {
-        let value = io::read_to_string(&int)?;
-        if value.trim_end() == "0" {
-            writeln!(lock)?;
-            return Ok(());
+fn read_content(back_cover: Variant) -> std::io::Result<Option<Vec<u8>>> {
+    match back_cover {
+        Variant::With256BBlocks(mut back_cover) => {
+            println!("TOH with memory chip (up to 16 blocks of 256 bytes) detected");
+            let content = back_cover.read_chip()?;
+            back_cover.power_down()?;
+            Ok(Some(content))
         }
-        int.rewind()?;
-        write!(lock, ".")?;
-        lock.flush()?;
-        sleep(Duration::from_millis(500));
-    }
-    Err(std::io::Error::new(
-        std::io::ErrorKind::TimedOut,
-        "TOH was not connected",
-    ))
-}
-
-/// Test ADC for the right type of chip
-fn test_adc_pin() -> Result<(), Box<dyn std::error::Error>> {
-    let adc = File::open(ADC_PATH)?;
-    let value = io::read_to_string(&adc)?.trim_end().parse::<u32>()?;
-    match value {
-        // TODO: Change this to use the production values!
-        600..=650 => {
-            println!("TOH with memory chip (8 blocks of 256 bytes) detected");
-            Ok(())
+        Variant::With64kBBlocks(back_cover) => {
+            println!("Unsupported TOH");
+            // TODO: Implement
+            back_cover.power_down()?;
+            Ok(None)
         }
-        _ => Err(format!("Unexpected value on ADC: {}", value)
-            .to_owned()
-            .into()),
+        Variant::Attached(back_cover) => {
+            println!("Unsupported TOH");
+            back_cover.power_down()?;
+            Ok(None)
+        }
     }
-}
-
-/// Enable or disable power
-fn set_power_with_silent(enable: bool, silent: bool) -> Result<(), std::io::Error> {
-    let mut pwr = File::create(PWR_PATH)?;
-    pwr.write_all(if enable { b"1" } else { b"0" })?;
-    if !silent {
-        println!("Power {}abled", if enable { "en" } else { "dis" });
-    }
-    sleep(Duration::from_millis(100));
-    Ok(())
-}
-
-/// Enable or disable power
-fn set_power(enable: bool) -> Result<(), std::io::Error> {
-    set_power_with_silent(enable, false)
-}
-
-/// Set I²C device address
-fn set_i2c_target_address(i2c: &mut File, address: u32) -> Result<(), std::io::Error> {
-    let raw_fd = i2c.as_raw_fd();
-    let result = unsafe { ioctl(raw_fd, I2C_SLAVE, address) };
-    if result < 0 {
-        Err(std::io::Error::last_os_error())?
-    }
-    Ok(())
-}
-
-/// Get file size
-/// Use I²C to read the chip into a vector
-fn read_chip(i2c: &mut File) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let mut result = Vec::new();
-    let mut lock = std::io::stdout().lock();
-    write!(lock, "Reading the chip for up to {} bytes", 256 * 8)?;
-    lock.flush()?;
-    let mut buf = [0; 256];
-    let mut read = 0_u64;
-    for address in 0x50..0x58 {
-        set_i2c_target_address(i2c, address)?;
-
-        // Set data address to zero
-        i2c.write_all(&[0])?;
-
-        i2c.read_exact(&mut buf)?;
-        result.extend(buf);
-
-        read += 256;
-        write!(lock, ".")?;
-        lock.flush()?;
-    }
-    writeln!(lock)?;
-    writeln!(lock, "{} bytes read", read)?;
-    Ok(result)
 }
 
 #[cfg(target_os = "linux")]
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Arguments = argh::from_env();
-    // TODO: Add page size argument for writing
-
-    // TODO: Modprobe i2c-dev if it is not there yet
-
     let mut file = File::options()
         .create(true)
+        .create_new(!args.overwrite)
         .write(true)
         .truncate(true)
         .open(args.output_file)?;
 
-    wait_for_int()?;
-
-    test_adc_pin()?;
-
-    set_power(true)?;
-
-    // Open the file for reading and writing
-    let mut i2c = File::options().read(true).write(true).open(I2C_PATH)?;
-
-    let result = read_chip(&mut i2c)?;
-    file.write_all(result.as_ref())?;
-
-    set_power(false)?;
+    println!("Waiting for TOH to be connected for up to a minute");
+    let back_cover = BackCover::new()?;
+    let back_cover = select! {
+        result = back_cover.wait_connect() => {
+            Ok(result?)
+        }
+        _ = sleep(Duration::from_secs(60)) => {
+            Err("No TOH detected")
+        }
+    }?;
+    match back_cover.power_up() {
+        Ok(variant) => {
+            if let Some(content) = read_content(variant)? {
+                println!("Read {} bytes from the chip", content.len());
+                file.write_all(content.as_slice())?;
+            } else {
+                return Err("TOH could not be read".into());
+            }
+        }
+        Err(error) => {
+            return Err(format!("TOH power up failed: {error}").into());
+        }
+    };
 
     Ok(())
 }
